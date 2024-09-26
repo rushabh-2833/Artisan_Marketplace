ef208cb740026fefae4a0e604297f836c03afa96
rushabh-2833 <rushabh4478@gmail.com>
2024-09-26 12:39:27 -0400

    Database Connection Setup

--- src/config/database.php
+++ src/config/database.php
@@ -1,10 +1,13 @@
 <?php
+
+
+
 $servername = "artisan-marketplace.cfao628yky31.us-east-1.rds.amazonaws.com";
 $username = "admin";
 $password = "Cap-Project24";
-$dbname = "artisan-marketplace";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+
+$conn = new mysqli($servername, $username, $password);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
