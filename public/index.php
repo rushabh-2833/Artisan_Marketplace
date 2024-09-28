0abbb41bc105feae20ebe8eae3361eb063c97a64
Divyang Savaliya <divyangsavaliya2002@gmail.com>
2024-09-28 11:19:49 -0400

    create a basic dummy featured products for now

--- public/index.php
+++ public/index.php
@@ -59,6 +59,28 @@
     </section>
 
 
+    <!-- Featured Products -->
+    <section class="featured-products my-5">
+        <div class="container">
+            <h2 class="text-center mb-4">Featured Products</h2>
+            <div class="row">
+                                
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="img/product1.jpeg" class="card-img-top" alt="Product Image">
+                        <div class="card-body">
+                            <h5 class="card-title">Save
+                                Star necklace,ceramic handmade </h5>
+                            <p class="card-text">$29.99</p>
+                            <a href="#" class="btn btn-primary btn-custom">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
 
  <!-- Bootstrap JS and Popper.js -->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
