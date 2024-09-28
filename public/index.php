ea645724c074e27e08908706fb2d9a9e8eb833f8
Divyang Savaliya <divyangsavaliya2002@gmail.com>
2024-09-28 11:23:47 -0400

    Basic footer section with some social media href in it

--- public/index.php
+++ public/index.php
@@ -150,7 +150,17 @@
         </div>
     </section>
 
-
+    <!-- Footer -->
+    <footer class="bg-dark text-light py-4">
+        <div class="container text-center">
+            <p>&copy; 2024 ArtisanHub. All Rights Reserved.</p>
+            <div class="social-links">
+                <a href="#" class="text-light me-3">Instagram</a>
+                <a href="#" class="text-light me-3">Twitter</a>
+                <a href="#" class="text-light me-3">Facebook</a>
+            </div>
+        </div>
+    </footer>
 
 
  <!-- Bootstrap JS and Popper.js -->
