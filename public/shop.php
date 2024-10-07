6d04a98182b8760871814b7813be90d78436433f
rushabh-2833 <rushabh4478@gmail.com>
2024-10-07 14:52:48 -0400

    Product_listing page created

---
+++ public/shop.php
@@ -0,0 +1,17 @@
+<table>
+    <tr>
+        <th>Product</th>
+        <th>Price</th>
+        <th>Image</th>
+    </tr>
+    <?php
+    $result = $conn->query("SELECT * FROM products WHERE status = 'Approved'");
+    while ($product = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>{$product['name']}</td>
+                <td>{$product['price']}</td>
+                <td><img src='{$product['image']}' width='100'></td>
+              </tr>";
+    }
+    ?>
+</table>
