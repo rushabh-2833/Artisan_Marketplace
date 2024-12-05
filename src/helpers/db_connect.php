0084214282245f7c28723a0715b1c3baead82dcf
rushabh-2833 <rushabh4478@gmail.com>
2024-12-05 13:58:38 -0500

    Remove vendor directory from Git and update .gitignore

--- src/helpers/db_connect.php
+++ src/helpers/db_connect.php
@@ -1,8 +1,14 @@
 <?php
-$servername = "artisan-marketplace.cfao628yky31.us-east-1.rds.amazonaws.com";
-$username = "admin";
-$password = "Cap-Project24";
-$dbname = "artisan_marketplace"; // Use the created database
+// Load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Access database connection details from .env
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+ // Use the created database
 
 // Connect to the database
 $conn = new mysqli($servername, $username, $password, $dbname);
